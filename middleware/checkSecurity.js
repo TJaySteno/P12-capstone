f532bd8685cfe4c6506e9b50a2b9e316f2743f73
Trent <trent.stenoien@gmail.com>
2018-08-06 01:40:56 -0500

    Better logic for checking for HTTPS

--- middleware/checkSecurity.js
+++ middleware/checkSecurity.js
@@ -1,7 +1,11 @@
 // Check if request is being made over HTTP or HTTPS
 const checkSecurity = async (req, res, next) => {
   try {
-    const https = req.headers.referer.includes('https://');
+    let https;
+
+    if (!req.headers.referer) https = false;
+    else https = req.headers.referer.includes('https://');
+
     req.options = { https };
     next();
   } catch (e) {
